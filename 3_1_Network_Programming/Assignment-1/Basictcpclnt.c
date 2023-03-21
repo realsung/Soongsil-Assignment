be217f8516d77897bed77f6c33343cc68e68aa0e
realsung <sungjun0208@naver.com>
2023-03-21 03:59:44 +0000

    upload network programming

---
+++ 3_1_Network_Programming/Assignment-1/Basictcpclnt.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+#define PORT 9001
+
+int main()		
+{		
+	int clntSd;	
+	struct sockaddr_in clntAddr;	
+	int clntAddrLen, readLen;	
+	char wBuff[] = "How old are you?";	
+	char rBuff[BUFSIZ];	
+		
+		
+		
+	clntSd = socket(PF_INET, SOCK_STREAM, 0);	
+	if(clntSd == -1)	
+	{	
+		printf("Socket Creation Error");
+		return -1;
+	}	
+	clntAddr.sin_family = AF_INET;	
+	clntAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	
+	clntAddr.sin_port = htons(PORT);	
+		
+	if(connect(clntSd, (struct sockaddr *) &clntAddr, sizeof(clntAddr)) == -1)
+	{	
+		printf("Connect Error");
+		close(clntSd);
+		return -1;
+	}
+	// ssize_t send(int sockfd, const void *buf, size_t len, int flags);
+	ssize_t send_status = send(clntSd, wBuff, sizeof(wBuff), 0);
+	printf("[*] send_status : %ld", send_status);
+	write(clntSd, wBuff, sizeof(wBuff));	
+	printf("Client: %s\n",wBuff);	
+		
+	readLen = read(clntSd, rBuff,sizeof(rBuff)-1);	
+	if(readLen == -1)	
+	{	
+		printf("Read Error");
+		return -1;
+	}	
+	rBuff[readLen] = '\0';
+	printf("Server: %s\n", rBuff);
+	close(clntSd);
+	return 0;
+}	
